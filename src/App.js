2adb19f6a69602d173ef8d8715ffd1e00f2ee800
Ryan <MiLaD.TechS@Gmail.Com>
2021-06-23 23:37:17 +0430

    removed test route

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
 import { AnimatePresence } from "framer-motion";
-import { Route, Switch, useLocation } from "react-router-dom";
+import { Switch, useLocation } from "react-router-dom";
 import AuthRoute from "./components/AuthRoute";
 import PrivateRoute from "./components/PrivateRoute";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
-import Test from "./pages/Test";
 import ValidateOTP from "./pages/ValidateOTP";
 import ValidatePhone from "./pages/ValidatePhone";
 
@@ -20,7 +19,6 @@ const App = () => {
         <AuthRoute path="/validate-phone" component={<ValidatePhone />} />
         <AuthRoute path="/validate-otp" component={<ValidateOTP />} />
         <AuthRoute path="/register" component={<Register />} />
-        <Route path="/test" component={Test} />
       </Switch>
     </AnimatePresence>
   );
